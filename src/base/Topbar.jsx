baa73d79f6921c270c15527ce1cd94d4ffc61485
eriveltosilva <eriveltoclenio@gmail.com>
2024-06-12 02:06:05 +0100

    refactor: navbar

--- src/base/Topbar.jsx
+++ src/base/Topbar.jsx
@@ -17,36 +17,21 @@ function Topbar() {
 
     return (
         <div className="container-fluid">
-            <div className="row bg-secondary py-1 px-xl-5">
-                <div className="col-lg-6 d-none d-lg-block">
-                    <div className="d-inline-flex align-items-center h-100">
-                    </div>
-                </div>
+            <div className="row bg-dark text-light py-1 px-xl-5">
                 <div className="col-lg-6 text-center text-lg-right">
-                    <div className="d-inline-flex align-items-center">
-                        <div className="btn-group">
-                            <button type="button" className="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">Minha conta</button>
-                            <div className="dropdown-menu dropdown-menu-right">
-                                <Link to={URL_ROUTES.LOGIN} className="dropdown-item">Login</Link>
-                                <Link to={URL_ROUTES.LOGOUT} className="dropdown-item">Logout</Link>
-                                <Link to={URL_ROUTES.REGISTER} className="dropdown-item">Criar Conta</Link>
-                            </div>
-                        </div>
-                        
-                    </div>
                     <div className="d-inline-flex align-items-center d-block d-lg-none">
-                        <Link to={URL_ROUTES.WISHLIST} className="btn px-0 ml-2">
+                        <Link to={URL_ROUTES.WISHLIST} className="btn px-0 ml-2" title='Ir para Favoritos'>
                             <i className="bi bi-heart-fill text-primary"></i>
-                            <span className="badge text-dark border border-dark rounded-circle" style={{paddingBottom: "2px"}}>0</span>
+                            <span className="badge text-light border border-light rounded-circle" style={{paddingBottom: "2px"}}>0</span>
                         </Link>
-                        <Link to={URL_ROUTES.GO_TO_CART} className="btn px-0 ml-2">
+                        <Link to={URL_ROUTES.GO_TO_CART} className="btn px-0 ml-2"  title='Ir para Carrinho'>
                             <i className="bi bi-cart-fill text-primary"></i>
-                            <span className="badge text-dark border border-dark rounded-circle" style={{paddingBottom: "2px"}}>0</span>
+                            <span className="badge text-light border border-light rounded-circle" style={{paddingBottom: "2px"}}>0</span>
                         </Link>
                     </div>
                 </div>
             </div>
-            <div className="row align-items-center bg-light py-3 px-xl-5 d-none d-lg-flex">
+            <div className="row align-items-center bg-dark text-light py-3 px-xl-5 d-none d-lg-flex">
                 <div className="col-lg-4">
                     <Link to={URL_ROUTES.ROOT} className="text-decoration-none">
                         <ImageLogo />
@@ -62,7 +47,7 @@ function Topbar() {
                             onKeyUp={(e)=>{(e.target.key=="Enter")?handleSearchForm(e):null}}
                             className="form-control" 
                             placeholder="Procure por produtos aqui..." />
-                            <button type='submit' style={{border:"none", backgroundColor:"#fff"}}>
+                            <button type='submit' style={{border:"none", backgroundColor:"transparent"}}>
                                 <div className="input-group-append">
                                     <span className="input-group-text bg-transparent text-primary">
                                         <i className="bi bi-search"></i>
@@ -73,8 +58,8 @@ function Topbar() {
                     </form>
                 </div>
                 <div className="col-lg-4 col-6 text-right">
-                    <p className="m-0">Apoio ao Cliente</p>
-                    <h5 className="m-0">+244 940 811 141</h5>
+                    <p className="m-0  text-light">Apoio ao Cliente</p>
+                    <h5 className="m-0  text-light">+244 940 811 141</h5>
                 </div>
             </div>
         </div>
--- src/components/MyMenuBar.jsx
+++ src/components/MyMenuBar.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import DefaultUserImage from '../assets/img/user.png';
 
 import apiInstance from '../utils/axios';
 import CartID from '../views/plugin/CartID';
@@ -9,20 +8,18 @@ import { URL_ROUTES } from '../utils/constants';
 import UserData from '../views/plugin/UserData';
 import { CartContext } from '../views/plugin/Context';
 
-import CategorySelector from './CategorySelector';
-
-import { Badge } from 'primereact/badge';
-import { Avatar } from 'primereact/avatar';
-import { Menubar } from 'primereact/menubar';
+import ImageLogo from './ImageLogo';
 
 function MyMenuBar() {
+
     const cartId = CartID();
     const userData = UserData();
-    
+
+
     const [cartCount, setCartCount] = useContext(CartContext);
-    const [imageProfile, setImageProfile] = useState(DefaultUserImage);
-    
-    
+
+    const [categories, setCategories] = useState([]);
+
     const fetchCartData = async () => {
         let url = userData ? `cart-list/${cartId}/${userData?.user_id}/` : `cart-list/${cartId}/`;
         await apiInstance.get(url).then((resp) => {
@@ -35,59 +32,113 @@ function MyMenuBar() {
     const fetchProfileData = async () => {
         if (userData?.user_id) {
             apiInstance.get(`customer/profile/${userData?.user_id}/`)
-            .then((res) => {
-                setImageProfile(res.data.image);
-            })
-            .catch((error) => {
-                console.error(error);
-            })
+                .then((res) => {
+                    setImageProfile(res.data.image);
+                })
+                .catch((error) => {
+                    console.error(error);
+                })
         }
     }
+
+
+    useEffect(() => {
+        apiInstance.get('categories/')
+            .then(resp => setCategories(resp.data))
+            .catch((error) => console.error(error))
+    }, []);
+
 
     useEffect(() => {
         fetchCartData();
         fetchProfileData();
     }, [])
 
-    const itemRenderer = (item) => (
-        <span className="flex flew-wrap align-items-center p-menuitem-link" >
-            <Link to={item.url} style={{ textDecoration: 'none' }} className='text-primary'>
-                <span className={item.icon} />
-                <span className="mx-2">{item.label}</span>
-                {item.badge !== undefined && <Badge className="ml-auto" value={item.badge} />}
-                {item.shortcut && <span className="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">{item.shortcut}</span>}
-            </Link>
-        </span>
-    );
-
-    const items = [
-        { "label": "Home", "url": URL_ROUTES.ROOT, template: itemRenderer },
-        { "label": "Novos Produtos", url: URL_ROUTES.NEW_PRODUCTS, template: itemRenderer },
-        { "label": "Destaques", url: URL_ROUTES.HIGHLIGHTS, template: itemRenderer },
-        // { "label": "Contactos", url: URL_ROUTES.CONTACTS, template: itemRenderer },
-        // {
-        //     "label": "Outras Secções", "items": [
-        //         { "label": 'Minha Conta', url: URL_ROUTES.CUSTOMER_ACCOUNT },
-        //         { "label": 'Sobre Nós', url: URL_ROUTES.ABOUT_US },
-        //         { "label": 'Faqs', url: URL_ROUTES.FAQS }], template: itemRenderer
-        // },
-        { "label": "Carrinho", icon: "pi pi-shopping-cart", url: URL_ROUTES.GO_TO_CART, badge: cartCount, template: itemRenderer },
-        // { "label": "Favoritos", icon: "pi pi-heart", url: URL_ROUTES.WISHLIST, badge: 0, template: itemRenderer },
-    ];
-
-    const end = (
-        <div className="flex align-items-center gap-2">
-            <Link to={URL_ROUTES.CUSTOMER_ACCOUNT}>
-                <Avatar image={imageProfile} shape="circle" />
-            </Link>
-        </div>
-    );
 
     return (
         <>
-            <div className="card">
-                <Menubar model={items} start={CategorySelector} className='w-full bg-dark' end={end} />
-            </div>
+            <div class="container-fluid bg-dark mb-30">
+                <div class="row px-xl-5">
+                    <div class="col-lg-3 d-none d-lg-block">
+                        <Link class="btn d-flex align-items-center justify-content-between bg-primary w-100 py-2" data-toggle="collapse" href="#navbar-vertical" style={{ height: "65px;", padding: "0 30px;" }}>
+                            <h6 class="text-dark m-0">
+                                <i class="bi bi-list mr-2"></i>
+                                Categorias
+                            </h6>
+                            <i class="bi bi-chevron-down text-dark"></i>
+                        </Link>
+                        <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 bg-light" id="navbar-vertical" style={{ width: "calc(100% - 30px)", zIndex: "999" }}>
+                            <div class="navbar-nav w-100">
+                                {
+                                    categories.length === 0 ?
+                                        <h2 className='h5 ml-4 text-muted'>Não foram encontrados nenhuma categoria de produto no nosso banco de dados😢.</h2>
+                                        :
+                                        <>
+                                            {
+                                                categories?.map((category) => (
+                                                    <Link to={`/products/category/${category?.slug}/`} class="nav-item nav-link">
+                                                        {category.title}
+                                                    </Link>
+                                                ))
+                                            }
+                                        </>
+                                }
+                            </div>
+                        </nav>
+                    </div>
+                    <div class="col-lg-9">
+                        <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">
+                            <Link to={URL_ROUTES.ROOT} class="text-decoration-none d-block d-lg-none">
+                                <ImageLogo />
+                            </Link>
+                            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                                <div class="navbar-nav mr-auto py-0">
+                                    <Link to={URL_ROUTES.ROOT} class="nav-item nav-link active">Home</Link>
+                                    <Link to={URL_ROUTES.NEW_PRODUCTS} class="nav-item nav-link">Novos Produtos</Link>
+                                    <Link to={URL_ROUTES.HIGHLIGHTS} class="nav-item nav-link">Destaques</Link>
+                                    {/* <Link to={URL_ROUTES.CONTACTS} class="nav-item nav-link">Contactos</Link> */}
+                                    <div class="nav-item dropdown">
+                                        <Link to={""} class="nav-link dropdown-toggle" data-toggle="dropdown">
+                                            Outras Secções
+                                        </Link>
+                                        <div class="dropdown-menu bg-primary rounded-0 border-0 m-0">
+                                            {userData === undefined ?
+                                                <Link to={URL_ROUTES.LOGIN} class="dropdown-item">Login</Link>
+                                                :
+                                                <>
+                                                    <Link to={URL_ROUTES.CUSTOMER_ACCOUNT} class="dropdown-item">Minha Conta</Link>
+                                                    <Link to={URL_ROUTES.LOGOUT} class="dropdown-item">Logout</Link>
+                                                </>
+                                            }
+                                            <Link to={URL_ROUTES.ABOUT_US} class="dropdown-item">Sobre nós</Link>
+                                            <Link to={URL_ROUTES.FAQS} class="dropdown-item">Faqs</Link>
+                                        </div>
+                                    </div>
+                                    <Link to={URL_ROUTES.GO_TO_CART} class="nav-item nav-link">Carrinho</Link>
+                                    {/* <Link to={URL_ROUTES.WISHLIST} class="nav-item nav-link">Favoritos</Link> */}
+                                </div>
+
+                                <div class="navbar-nav ml-auto py-0 d-none d-lg-block">
+                                    <Link to={URL_ROUTES.WISHLIST} class="btn px-0">
+                                        <i class="bi bi-heart-fill text-primary"></i>
+                                        <span class="badge text-secondary border border-secondary rounded-circle" style={{ paddingBottom: "2px" }}>
+                                            {cartCount}
+                                        </span>
+                                    </Link>
+
+                                    {/* <Link to={URL_ROUTES.GO_TO_CART} class="btn px-0 ml-3">
+                                        <i class="bi bi-cart-fill text-primary"></i>
+                                        <span class="badge text-secondary border border-secondary rounded-circle" style={{ paddingBottom: "2px" }}>0</span>
+                                    </Link> */}
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div >
+            </div >
         </>
     )
 }
